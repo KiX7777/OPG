93215d78520947e5847a99a55aa27e380677f20a
KiX7777 <kristianboi202@gmail.com>
2023-05-03 09:57:29 +0200

    added form sending

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import classes from '../pages/Contact.module.css';
+import { ScaleLoader } from 'react-spinners';
 
 const ContactForm = () => {
   const [name, setname] = useState('');
@@ -9,6 +10,7 @@ const ContactForm = () => {
   const [nameTouched, setnameTouched] = useState(false);
   const [emailTouched, setemailTouched] = useState(false);
   const [textTouched, settextTouched] = useState(false);
+  const [isSending, setisSending] = useState(false);
   const isEmail = (email) =>
     /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
@@ -22,17 +24,47 @@ const ContactForm = () => {
   const emailError = !emailIsValid && emailTouched;
   const textError = !textIsValid && textTouched;
 
+  async function sendMessage(message) {
+    setisSending(true);
+    try {
+      const res = await fetch(
+        'https://opg-373814-default-rtdb.firebaseio.com/messages.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(message),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (!res.ok) {
+        throw new Error('Sending message failed!');
+      }
+      const data = await res.json();
+      console.log(data);
+      navigate('/home', { state: 'Thank you for your message.' });
+    } catch (err) {
+      console.error(err);
+    }
+    setisSending(false);
+  }
+
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    const message = {
+      name,
+      email,
+      msg: text,
+    };
+
     if (formValid) {
       console.log(name, email, text);
+      sendMessage(message);
       setname('');
       setemail('');
       settext('');
-
-      navigate('/home', { state: 'Thank you for your message.' });
     } else {
       setnameTouched(true);
       setemailTouched(true);
@@ -41,63 +73,72 @@ const ContactForm = () => {
   }
 
   return (
-    <form>
-      <div className={classes.formContainer}>
-        <label htmlFor='name'>Name</label>
-        <input
-          id='name'
-          type='text'
-          minLength={3}
-          value={name}
-          placeholder='Your name'
-          onChange={(e) => {
-            setname(e.target.value);
-          }}
-          onBlur={() => {
-            setnameTouched(true);
-          }}
-        ></input>
-        {nameError && <small className='errorMsg'>Enter your name.</small>}
-      </div>
-      <div className={classes.formContainer}>
-        <label htmlFor='email'>Email</label>
-        <input
-          id='email'
-          placeholder='Your email'
-          type='email'
-          value={email}
-          onChange={(e) => {
-            setemail(e.target.value);
-          }}
-          onBlur={() => {
-            setemailTouched(true);
-          }}
-        ></input>
-        {emailError && <small className='errorMsg'>Enter your email.</small>}
-      </div>
+    <div className={classes.formsContainer}>
+      {isSending && (
+        <ScaleLoader color='white' className={classes.formLoader} />
+      )}
+      {!isSending && (
+        <form>
+          <div className={classes.formContainer}>
+            <label htmlFor='name'>Name</label>
+            <input
+              id='name'
+              type='text'
+              minLength={3}
+              value={name}
+              placeholder='Your name'
+              onChange={(e) => {
+                setname(e.target.value);
+              }}
+              onBlur={() => {
+                setnameTouched(true);
+              }}
+            ></input>
+            {nameError && <small className='errorMsg'>Enter your name.</small>}
+          </div>
+          <div className={classes.formContainer}>
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              placeholder='Your email'
+              type='email'
+              value={email}
+              onChange={(e) => {
+                setemail(e.target.value);
+              }}
+              onBlur={() => {
+                setemailTouched(true);
+              }}
+            ></input>
+            {emailError && (
+              <small className='errorMsg'>Enter your email.</small>
+            )}
+          </div>
 
-      <div className={classes.formContainer}>
-        <label htmlFor='text'>Message</label>
-        <textarea
-          value={text}
-          id='textarea'
-          placeholder='Write your message for us.'
-          onChange={(e) => {
-            settext(e.target.value);
-          }}
-          onBlur={() => {
-            settextTouched(true);
-          }}
-        ></textarea>
-        {textError && (
-          <small className='errorMsg'>
-            Enter your message that you want to send.
-          </small>
-        )}
-      </div>
+          <div className={classes.formContainer}>
+            <label htmlFor='text'>Message</label>
+            <textarea
+              value={text}
+              id='textarea'
+              placeholder='Write your message for us.'
+              onChange={(e) => {
+                settext(e.target.value);
+              }}
+              onBlur={() => {
+                settextTouched(true);
+              }}
+            ></textarea>
+            {textError && (
+              <small className='errorMsg'>
+                Enter your message that you want to send.
+              </small>
+            )}
+          </div>
 
-      <button onClick={handleSubmit}>Send Message</button>
-    </form>
+          <button onClick={handleSubmit}>Send Message</button>
+        </form>
+      )}
+    </div>
   );
 };
 export default ContactForm;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+import classes from './About.module.css';
+import { ScaleLoader } from 'react-spinners';
+
 const About = () => {
+  const [loadMap, setLoadMap] = useState(true);
   return (
-    <>
+    <div className={classes.aboutContainer}>
       <h1>About</h1>
-    </>
+      <div className={classes.mapContainer}>
+        <div style={{ width: '100%', height: '50dvh' }}>
+          {loadMap && (
+            <ScaleLoader
+              color='white'
+              style={{
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)',
+              }}
+            />
+          )}
+          <iframe
+            width='100%'
+            height='100%'
+            onLoad={() => {
+              setLoadMap((prev) => !prev);
+            }}
+            style={{
+              borderRadius: '10px',
+              boxShadow: '0 2px 10px 5px rgba(0,0,0,0.2)',
+            }}
+            frameBorder='0'
+            scrolling='no'
+            marginHeight='0'
+            marginWidth='0'
+            src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Josipa%20Martinca%2027+(OPG)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
+          >
+            <a href='https://www.maps.ie/distance-area-calculator.html'>
+              measure area map
+            </a>
+          </iframe>
+        </div>
+      </div>
+    </div>
   );
 };
 
